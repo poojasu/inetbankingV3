bd58877097628712ca248f220a224171a03dcbb2
poojasu <poojasubhasho55@gmail.com>
2021-10-29 19:24:03 +0530

    this is my 2nd commit

--- src/test/java/com/inetbanking/testCases/BaseClass.java
+++ src/test/java/com/inetbanking/testCases/BaseClass.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
